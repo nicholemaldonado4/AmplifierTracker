6e9b2eaf9bb38884ccef09855298e5b5d4432c9c
Nichole Maldonado <nicholemaldonado4@gmail.com>
2021-03-24 22:47:40 -0600

    Removed old uncommented code

--- header.php
+++ header.php
@@ -26,9 +26,7 @@
         <span class="logo">Amplifier Tracker</span>
         <span class="toggle drop-down"><i class="fa fa-bars fa-3x"></i></span>
     </div>
-    <!-- TODO: fix class k. Right now toggling for active. which hides/displays nav-->
     <ul class="nav_bar_list">
-<!--        <li class="logo"><a href="#">Amplifier Tracker</a></li>-->
         <li class="item"><a href="/index.php">Home</a></li>
         <li class="item<?=showWelcome();?>"><a href="/amps-ants.php">Antennas and Amplifiers</a></li>
         <li class="item<?=showWelcome();?>"><a href="/maintenance/search.php">Maintenance Search</a></li>
@@ -39,6 +37,5 @@
                 <li class="subitem" id="logOutBtn"><a href="#">Sign Out</a></li>
             </ul>
         </li>
-<!--        <li class="toggle"><a href="#"><i class="fa fa-bars fa-3x"></i></a></li>-->
     </ul>
 </nav>
